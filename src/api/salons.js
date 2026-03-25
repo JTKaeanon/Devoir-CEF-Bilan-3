0de037271c90c176e5b416f12ef8e2e7cef74333
Jessie Texier <jessitexier8@gmail.com>
2026-03-25 14:13:29 +0100

    page temporaire details

--- src/api/salons.js
+++ src/api/salons.js
@@ -2,17 +2,33 @@
 import imgSalon1 from '../assets/salon1.jpg';
 import imgSalon2 from '../assets/salon2.jpg';
 import imgSalon3 from '../assets/salon3.jpg';
+import imgBarberCut from '../assets/client-doing-hair-cut-barber-shop-salon.jpg';
+
 
 export const salonsData = [
   {
     id: 'the-old-school',
     name: 'The Old School',
-    subtitle: "Chic & Contemporain",
-    target: 'Homme et enfant ',
+    subtitle: "L'art du rasage traditionnel, le style en plus.",
+    target: 'Homme et enfant',
     address: '12 Rue de la Monnaie, 35000 Rennes',
     phone: '02 99 31 45 67',
     image: imgSalon1,
-    description: "Un espace dédié à l'élégance intemporelle au cœur du centre historique de Rennes."
+    presentationImage: imgBarberCut, // <-- L'image de fond pour le bloc en haut à droite
+    description: "Un espace dédié à l'élégance intemporelle au cœur du centre historique de Rennes.",
+    // NOUVEAU : L'équipe
+    equipe: [
+        { id: 1, nom: 'Carte\nCoiffeur' },
+        { id: 2, nom: 'Carte\nCoiffeur' },
+        { id: 3, nom: 'Carte\nCoiffeur' },
+        { id: 4, nom: 'Carte\nCoiffeur' }
+    ],
+    // NOUVEAU : Les services
+    prestations: [
+        { id: 1, nom: 'Coupe Homme (avec ou sans shampoing)', duree: '30 Minutes', prix: '25 €' },
+        { id: 2, nom: 'Barbier (Taille de barbe)', duree: '30 Minutes', prix: '22 €' },
+        { id: 3, nom: 'Formule Coupe + Barbe', duree: '1 Heure', prix: '42 €' }
+    ]
   },
   {
     id: 'lessence-de-soi',
